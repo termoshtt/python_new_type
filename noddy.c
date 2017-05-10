463b17ed7af67738da82981a164f0ba048c9124a
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-05-10 14:32:03 +0900

    Add data members and new/init/dealloc

--- noddy.c
+++ noddy.c
@@ -1,16 +1,100 @@
 
 #include <Python.h>
+#include <structmember.h>
 
 typedef struct {
-  PyObject_HEAD
-  /* Type-specific fields go here. */
-} noddy_NoddyObject;
+  PyObject_HEAD PyObject *first;
+  PyObject *last;
+  int number;
+} Noddy;
 
-static PyTypeObject noddy_NoddyType = {
+static void Noddy_dealloc(Noddy *self) {
+  Py_XDECREF(self->first);
+  Py_XDECREF(self->last);
+  Py_TYPE(self)->tp_free((PyObject *)self);
+}
+
+static PyObject *Noddy_new(PyTypeObject *type, PyObject *args, PyObject *kwds) {
+  Noddy *self;
+
+  self = (Noddy *)type->tp_alloc(type, 0);
+  if (self != NULL) {
+    self->first = PyUnicode_FromString("");
+    if (self->first == NULL) {
+      Py_DECREF(self);
+      return NULL;
+    }
+
+    self->last = PyUnicode_FromString("");
+    if (self->last == NULL) {
+      Py_DECREF(self);
+      return NULL;
+    }
+
+    self->number = 0;
+  }
+
+  return (PyObject *)self;
+}
+
+static int Noddy_init(Noddy *self, PyObject *args, PyObject *kwds) {
+  PyObject *first = NULL, *last = NULL, *tmp;
+
+  static char *kwlist[] = {"first", "last", "number", NULL};
+
+  if (!PyArg_ParseTupleAndKeywords(args, kwds, "|OOi", kwlist, &first, &last,
+                                   &self->number))
+    return -1;
+
+  if (first) {
+    tmp = self->first;
+    Py_INCREF(first);
+    self->first = first;
+    Py_XDECREF(tmp);
+  }
+
+  if (last) {
+    tmp = self->last;
+    Py_INCREF(last);
+    self->last = last;
+    Py_XDECREF(tmp);
+  }
+
+  return 0;
+}
+
+static PyMemberDef Noddy_members[] = {
+    {"first", T_OBJECT_EX, offsetof(Noddy, first), 0, "first name"},
+    {"last", T_OBJECT_EX, offsetof(Noddy, last), 0, "last name"},
+    {"number", T_INT, offsetof(Noddy, number), 0, "noddy number"},
+    {NULL} /* Sentinel */
+};
+
+static PyObject *Noddy_name(Noddy *self) {
+  if (self->first == NULL) {
+    PyErr_SetString(PyExc_AttributeError, "first");
+    return NULL;
+  }
+
+  if (self->last == NULL) {
+    PyErr_SetString(PyExc_AttributeError, "last");
+    return NULL;
+  }
+
+  return PyUnicode_FromFormat("%S %S", self->first, self->last);
+}
+
+static PyMethodDef Noddy_methods[] = {
+    {"name", (PyCFunction)Noddy_name, METH_NOARGS,
+     "Return the name, combining the first and last name"},
+    {NULL} /* Sentinel */
+};
+
+static PyTypeObject NoddyType = {
     PyVarObject_HEAD_INIT(NULL, 0) "noddy.Noddy", /* tp_name */
-    sizeof(noddy_NoddyObject),                    /* tp_basicsize */
+    sizeof(Noddy),                                /* tp_basicsize */
     0,                                            /* tp_itemsize */
-    0,                                            /* tp_dealloc */
+    (destructor)Noddy_dealloc,                    /* tp_dealloc */
     0,                                            /* tp_print */
     0,                                            /* tp_getattr */
     0,                                            /* tp_setattr */
@@ -25,13 +109,30 @@ static PyTypeObject noddy_NoddyType = {
     0,                                            /* tp_getattro */
     0,                                            /* tp_setattro */
     0,                                            /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,                           /* tp_flags */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,     /* tp_flags */
     "Noddy objects",                              /* tp_doc */
+    0,                                            /* tp_traverse */
+    0,                                            /* tp_clear */
+    0,                                            /* tp_richcompare */
+    0,                                            /* tp_weaklistoffset */
+    0,                                            /* tp_iter */
+    0,                                            /* tp_iternext */
+    Noddy_methods,                                /* tp_methods */
+    Noddy_members,                                /* tp_members */
+    0,                                            /* tp_getset */
+    0,                                            /* tp_base */
+    0,                                            /* tp_dict */
+    0,                                            /* tp_descr_get */
+    0,                                            /* tp_descr_set */
+    0,                                            /* tp_dictoffset */
+    (initproc)Noddy_init,                         /* tp_init */
+    0,                                            /* tp_alloc */
+    Noddy_new,                                    /* tp_new */
 };
 
-static PyModuleDef noddymodule = {
+static PyModuleDef noddy_module = {
     PyModuleDef_HEAD_INIT,
-    "noddy",
+    "noddy2",
     "Example module that creates an extension type.",
     -1,
     NULL,
@@ -43,15 +144,14 @@ static PyModuleDef noddymodule = {
 PyMODINIT_FUNC PyInit_noddy(void) {
   PyObject *m;
 
-  noddy_NoddyType.tp_new = PyType_GenericNew;
-  if (PyType_Ready(&noddy_NoddyType) < 0)
+  if (PyType_Ready(&NoddyType) < 0)
     return NULL;
 
-  m = PyModule_Create(&noddymodule);
+  m = PyModule_Create(&noddy_module);
   if (m == NULL)
     return NULL;
 
-  Py_INCREF(&noddy_NoddyType);
-  PyModule_AddObject(m, "Noddy", (PyObject *)&noddy_NoddyType);
+  Py_INCREF(&NoddyType);
+  PyModule_AddObject(m, "Noddy", (PyObject *)&NoddyType);
   return m;
 }
